c2efbf51622135faf7af192c2548f78e68497fdc
Budhil-S <122159812+Budhil-S@users.noreply.github.com>
2023-09-04 12:19:19 +0530

    Update Palindromic Substrings

--- Palindromic Substrings.py
+++ Palindromic Substrings.py
@@ -1,16 +1,14 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        count = 0
-        for i in range (0,len(s)):
-            left,right = i,i
+        def countPalindromes(s,left,right) -> int:
+            count = 0
             while left >=0 and right < len(s) and s[left] == s[right]:
                 count += 1
                 left -= 1
                 right += 1
-                
-            left, right = i,i+1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                count += 1
-                left -= 1
-                right += 1
+            return count
+        count = 0
+        for i in range (0,len(s)):
+           count += countPalindromes(s,i,i)
+           count += countPalindromes(s,i,i+1)
         return count
